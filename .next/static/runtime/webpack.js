4d9d2c077b425c97af555d8b0b47865088cfecb0
kru1980 <kru71101@gmail.com>
2019-12-09 19:36:45 +0500

    091219

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ef8589c364b282e29e5b";
+/******/ 	var hotCurrentHash = "7bf55e19d642a1a3585e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
